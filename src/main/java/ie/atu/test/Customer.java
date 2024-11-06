65e3c6cc46d5a2c9f09fec646889dd3d97805a0f
CarlosMDSN <G00429041@atu.ie>
2024-11-06 16:15:33 +0000

    1st commit

---
+++ src/main/java/ie/atu/test/Customer.java
@@ -0,0 +1,16 @@
+package ie.atu.test;
+
+public class Customer {
+    private String customerId;
+    private String Name;
+    private String Email;
+    private String phoneNumber;
+
+    public customerId(){
+
+    }
+
+    public customerId(String ){
+
+    }
+}
